c8076cf4bce052d13ff225253f934cf2a691279e
Simon Agbey <fadecsolution@gmail.com>
2022-01-20 14:23:05 +0100

    second commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import  { ContactForm }  from './components/ContactForm.jsx';
+import  { ContactList } from './components/ContactList.jsx';
+import  { Header } from './components/Header.jsx';
+
+export const App = () => (
+  <div>
+    <Header />
+     <div className='min-h-full'>
+      <div className='maz-w-4xl mx-auto p-2'> 
+       <ContactForm />
+     <ContactList />
+    </div>
+   </div>
+  </div>
+);
